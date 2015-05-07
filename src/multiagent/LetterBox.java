640fee51c3bb6c89651cdfc59bca9f5524a9bedb
Therberus <stuhallifax@live.co.uk>
2015-05-07 14:59:45 +0200

    Added LetterBox class

--- src/multiagent/LetterBox.java
+++ src/multiagent/LetterBox.java
@@ -6,7 +6,23 @@ import java.util.HashMap;
 public class LetterBox {
 
 	HashMap<Integer, ArrayList<Message>> letterBox;
+
+	public LetterBox(int numberOfAgents) {
+		for(int i=0; i<numberOfAgents; i++){
+			letterBox.put(i, new ArrayList<Message>());
+		}
+	}
 	
+	public void putMessage(int destination, Message message){
+		letterBox.get(destination).add(message);
+	}
 	
+	public ArrayList<Message> getMessages(int agent){
+		return letterBox.get(agent);
+	}
 	
+	public void deleteMessages(int agent){
+		letterBox.put(agent, new ArrayList<Message>());
+	}
+
 }
