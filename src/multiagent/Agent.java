559b7b8387701378b39ad436b88959aea86df24a
gus3000 <gus3000spam@gmail.com>
2015-05-07 15:00:25 +0200

    Coordonnées agent

--- src/multiagent/Agent.java
+++ src/multiagent/Agent.java
@@ -5,8 +5,19 @@ public class Agent
 	private static int LASTID = -1;
 	public final int ID;
 
-	public Agent()
+	private int currentX;
+	private int currentY;
+
+	private int goalX;
+	private int goalY;
+
+	public Agent(int currentX, int currentY, int goalX, int goalY)
 	{
 		ID = ++LASTID;
+		this.currentX = currentX;
+		this.currentY = currentY;
+		this.goalX = goalX;
+		this.goalY = goalY;
 	}
+
 }
